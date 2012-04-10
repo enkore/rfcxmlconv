a4dd5dde4401f2d927f941963ff2bdf6f1c76645
enkore <public@enkore.de>
2012-04-10 11:52:24 +0200

    Added recursion for nesting and fixed some stuff

--- main.py
+++ main.py
@@ -18,6 +18,7 @@
 import argparse
 import datetime
 import re
+import sys
 from xml.etree import ElementTree
 from xml.sax.saxutils import escape
 from cStringIO import StringIO
@@ -150,6 +151,31 @@ class MDOutput(Output):
 	def __init__(self):
 		self.o = StringIO()
 
+	def _trim(self, docstring):
+		if not docstring:
+			return ''
+		# Convert tabs to spaces (following the normal Python rules)
+		# and split into a list of lines:
+		lines = docstring.expandtabs().splitlines()
+		# Determine minimum indentation (first line doesn't count):
+		indent = sys.maxint
+		for line in lines[1:]:
+			stripped = line.lstrip()
+			if stripped:
+				indent = min(indent, len(line) - len(stripped))
+		# Remove indentation (first line is special):
+		trimmed = [lines[0].strip()]
+		if indent < sys.maxint:
+			for line in lines[1:]:
+				trimmed.append(line[indent:].rstrip())
+		# Strip off trailing and leading blank lines:
+		while trimmed and not trimmed[-1]:
+			trimmed.pop()
+		while trimmed and not trimmed[0]:
+			trimmed.pop(0)
+		# Return a single string:
+		return '\n'.join(trimmed)
+
 	def _escape(self, text):
 		if type(text).__name__ == "dict":
 			for key, item in text.items():
@@ -158,7 +184,7 @@ class MDOutput(Output):
 			for i, item in enumerate(text):
 				text[i] = self._escape(item)
 		if type(text).__name__ == "str" or type(text).__name__ == "unicode":
-			text = escape(text)
+			text = self._trim(escape(text))
 		return text
 
 	def getvalue(self):
@@ -171,7 +197,7 @@ class MDOutput(Output):
 			at += "- [%(fullname)s](%(uri)s) - %(email)s" % author
 		data["authors"] = at
 
-		self.o.write("""# %(title)s
+		self.o.write("""# %(rfc)s - %(title)s
 **%(area)s** - Workgroup: %(workgroup)s
 
 Authors
@@ -185,21 +211,32 @@ Authors
 		self.o.write("#" * (level+2) + " " + self._escape(title) + "\n")
 		
 		for element in list(text):
-			if element.tag == "section":
-				continue
+			self._do_element(element)
 
-			if element.tag == "t" and element.text:
-				self.o.write(self._escape(element.text) + "\n")
-			if element.tag == "list":
-				style = "- "
-				if element.get("style") == "numbers":
-					style = "1. "
-				for point in list(element):
-					self.o.write(style + self._escape(point.text) + "\n")
-			if element.tag == "texttable":
-				self.o.write("(Here would be a table, but markdown doesn't support tables. Sorry!\n")
+	def _do_element(self, element):
+		if element.tag == "t" and element.text:
+			self.o.write(self._escape(element.text) + "\n")
+			[self._do_element(e) for e in list(element)]
+		if element.tag == "xref":
+			self.o.write("[%s](http://www.ietf.org/rfc/%s.txt)" % (element.text, element.get("target").lower()))
+		if element.tag == "list":
+			style = "- "
+			if element.get("style") == "numbers":
+				style = "1. "
+			for point in list(element):
+				text = self._escape(reduce(lambda u, v: u+v, [f for f in point.itertext()]))
+				if element.get("style") == "hanging":
+					self.o.write(style + "**%s** %s\n" % (self._escape(point.get("hangText")), text))
+				else:
+					self.o.write(style + text + "\n")
+		if element.tag == "figure":
+			for line in element.find("artwork").text.splitlines():
+				if len(line):
+					self.o.write("`" + line + "`\n\n")
+		if element.tag == "texttable":
+			self.o.write("(Here would be a table, but markdown doesn't support tables. Sorry!\n")
 
-			self.o.write("\n")
+		self.o.write("\n")
 
 
 class RFCParser():
@@ -214,6 +251,12 @@ class RFCParser():
 		self.o.Metadata(self.collect_metadata())
 		self.walk()
 
+	def _checked_text(self, e):
+		if e == None:
+			return ""
+		else:
+			return e.text
+
 	def parse_text(self, elements):
 		return reduce(lambda u, v: u+v, [e.text for e in elements])
 
@@ -224,15 +267,16 @@ class RFCParser():
 		for author in self.title.findall("author"):
 			authors.append({
 				"fullname": author.get("fullname"),
-				"organization": author.find("organization").text,
-				"email": author.find("address/email").text,
-				"uri": author.find("address/uri").text
+				"organization": self._checked_text(author.find("organization")),
+				"email": self._checked_text(author.find("address/email")),
+				"uri": self._checked_text(author.find("address/uri")),
 			})
 		md["authors"] = authors
 		#md["date"] = datetime.now()
 		md["area"]  = self.title.find("area").text
 		md["workgroup"] = self.title.find("workgroup").text
 		md["abstract"] = self.parse_text(self.title.find("abstract").findall("t"))
+		md["rfc"] = "RFC X" + self.root.get("number")
 
 		return self.o._escape(md)
 
